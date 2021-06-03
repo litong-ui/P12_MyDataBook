c3d515dcdc65b8d3554427a9eba6e544fcb232e9
19029489 <19029489@rp.edu.sg>
2021-06-03 14:57:12 +0800

    intermediate enhancement completed

--- app/src/main/java/sg/edu/rp/webservices/p12_mydatabook/AboutUsActivity.java
+++ app/src/main/java/sg/edu/rp/webservices/p12_mydatabook/AboutUsActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 
 public class AboutUsActivity extends AppCompatActivity {
@@ -24,21 +25,11 @@ public class AboutUsActivity extends AppCompatActivity {
         iv = findViewById(R.id.imageView);
 
         String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/8/80/Republic_Polytechnic_Logo.jpg";
-        Picasso.with(this).load(imageUrl).into(iv);
 
-        Picasso.with(this).load(imageUrl)
-                .into(iv, new com.squareup.picasso.Callback() {
-                    @Override
-                    public void onSuccess() {
-                        if (progressBar != null) {
-                            progressBar.setVisibility(View.GONE);
-                        }
-                    }
-
-                    @Override
-                    public void onError() {
-
-                    }
-                });
+        Picasso.with(this)
+                .load(imageUrl)
+                .placeholder(R.drawable.ajax_loader)
+                .error(R.drawable.error)
+                .into(iv);
     }
 }
